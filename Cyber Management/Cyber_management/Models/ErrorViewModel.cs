704923fb61043f7e44b5fb7b6bf36585ec3422bf
hatpeka <89801820+hatpeka@users.noreply.github.com>
2023-11-14 09:55:20 +0700

    Project set up

---
+++ Cyber Management/Cyber_management/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Cyber_management.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
